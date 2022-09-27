48e55b7a034721e1cae9f979ce3193ae23fa4e68
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-09-27 16:48:06 +0530

    Add Navigations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/Navigation";
 import Footer from "./components/Footer";
+import Outlets from "./pages/Outlets";
 import Outlet from "./pages/Outlet";
 // import "bootstrap/dist/css/bootstrap.css";
 import "./styles/master.css";
@@ -13,6 +14,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<h1>Home Component</h1>} />
+          <Route path="/Outlets" element={<Outlets />} />
           <Route path="/Outlet" element={<Outlet />} />
           <Route path="/Foods" element={<h1>Foods Component</h1>} />
           <Route path="/Wish-List" element={<h1>Wish-List Component</h1>} />
