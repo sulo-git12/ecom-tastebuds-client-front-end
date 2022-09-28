e0655d129e5c5e098e9c1ae42553fe40dd012bcf
sulo-git12 <sulo-git12@DESKTOP-O7FKKKV>
2022-09-29 00:03:11 +0530

    food shop

--- src/components/FoodInShop.js
+++ src/components/FoodInShop.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import FoodItemCard from './cards/FoodItemCard';
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 export default function FoodInShop() {
   return (
