4fabfe31c8c7bfeafc40511882d232191dcedb1e
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-10-09 11:16:39 +0530

    import some pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
 import Outlets from "./pages/Outlets";
 import Outlet from "./pages/Outlet";
-import MyOrders from "./pages/MyOrdersList";
-import Itemcart from "./pages/ItemCart";
-import Footer from "./components/Footer";
-// import "bootstrap/dist/css/bootstrap.css";
-import "./styles/master.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-          <NavigationBar />
-          <Routes>
-            <Route path="/" element={<h1>Home Component</h1>} />
-            <Route path="/Outlets" element={<Outlets />} />
-            <Route path="/Outlet" element={<Outlet />} />
-            <Route path="/Foods" element={<h1>Foods Component</h1>} />
-            <Route path="/Wish-List" element={<h1>Wish-List Component</h1>} />
-            <Route path="/Profile" element={<h1>Profile Component</h1>} />
-            <Route path="/itemcart" element={<Itemcart />} />
-            <Route path="/myOrders" element={<MyOrders />} />
-          </Routes>
+        <NavigationBar />
+
+        <Routes>
+          <Route path="/" element={<h1>Home Component</h1>} />
+          <Route path="/Outlets" element={<Outlets />} />
+          <Route path="/Outlets/:_id" element={<Outlet />} />
+          <Route path="/Foods" element={<h1>Foods Component</h1>} />
+          <Route path="/Wish-List" element={<h1>Wish-List Component</h1>} />
+          <Route path="/Profile" element={<h1>Profile Component</h1>} />
+        </Routes>
       </BrowserRouter>
-     
+
       {/* <Footer /> */}
     </div>
   );
