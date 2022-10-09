10c6d6431e3299823a2abed1500264194a07e786
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-10-09 11:59:31 +0530

    Page Navigation Issue fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
-import Footer from "./components/Footer";
+import NavigationBar from "./components/Layout/NavigationBar";
+import Footer from "./components/Layout/Footer";
 import Outlets from "./pages/Outlets";
+import MainOutlets from "./pages/MainOutlets";
+import FoodOutlets from "./pages/FoodOutlet";
+import FavFoodOutlets from "./pages/FavFoodOutletList";
 import Outlet from "./pages/Outlet";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/master.css";
 
 const App = () => {
   return (
@@ -15,9 +19,10 @@ const App = () => {
         <Routes>
           <Route path="/" element={<h1>Home Component</h1>} />
           <Route path="/Outlets" element={<Outlets />} />
-          <Route path="/Outlets/:_id" element={<Outlet />} />
+          <Route path="/MainOutlets" element={<MainOutlets />} />
+          <Route path="/FoodOutlets/:outletId" element={<FoodOutlets />} />
           <Route path="/Foods" element={<h1>Foods Component</h1>} />
-          <Route path="/Wish-List" element={<h1>Wish-List Component</h1>} />
+          <Route path="/WishList" element={<FavFoodOutlets />} />
           <Route path="/Profile" element={<h1>Profile Component</h1>} />
         </Routes>
       </BrowserRouter>
--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "../styles/master.css";
 
 const Footer = () => {
   return (
