644f7015c29d8df21d97a7bb844a36292d395ce1
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-10-11 15:45:49 +0530

    Change routings

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import NavigationBar from "./components/Layout/NavigationBar";
 // import Footer from "./components/Layout/Footer";
 import Outlets from "./pages/Outlets";
 import Foods from "./pages/Foods";
-import MainOutlets from "./pages/MainOutlets";
 import FoodOutlets from "./pages/FoodOutlet";
 import FavFoodOutlets from "./pages/FavFoodOutletList";
 import ItemCart from "./pages/ItemCart";
@@ -23,9 +22,8 @@ root.render(
   <Router>
     <NavigationBar />
     <Routes>
-      <Route path="/" element={<Outlets />} />
+      <Route path="/TasteBuds" element={<Outlets />} />
       <Route path="/Outlet/:_id/Foods" element={<Foods />} />
-      <Route path="/MainOutlets" element={<MainOutlets />} />
       <Route path="/FoodOutlets/:outletId" element={<FoodOutlets />} />
       <Route path="/Favourites" element={<FavFoodOutlets />} />
       <Route path="/Itemcart" element={<ItemCart />} />
