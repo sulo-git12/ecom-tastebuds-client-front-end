8a6a66311a595c4d376b6b16bac532feae4fd904
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-10-15 08:36:22 +0530

    conflit issue fixed

--- src/components/Layout/NavigationBar.js
+++ src/components/Layout/NavigationBar.js
@@ -3,74 +3,64 @@ import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Navigation = () => {
-  
   const { loginWithRedirect } = useAuth0();
   const { logout } = useAuth0();
   const { isAuthenticated } = useAuth0();
   const { user } = useAuth0();
 
   return (
-    
-      <div>
-        <ul className="nav-ul">
-          <li>
-              {
-                isAuthenticated ? (
-                        <>
-                          <Link key={0} className="brand-name" to="/TasteBuds">
-                            TasteBuds
-                          </Link>
-                          <Link key={1} to="/Favourites">
-                            Wish-List
-                          </Link>
-                          <Link key={2} to="/Itemcart">
-                            Itemcart (2)
-                          </Link>
-                          <Link key={3} to="/MyOrders">
-                            My Orders
-                          </Link>
-                       </>
-                ):(
-                      <>
-                         <Link key={0} className="brand-name" to="/TasteBuds">
-                          TasteBuds
-                        </Link>
-                      </>
-                )
-              }   
-          </li>
-            { isAuthenticated && <li> <p className="username">&nbsp;&nbsp; User : {user.name} </p></li> }
-          <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>  
-            {
-              isAuthenticated ? (<li><button className="logout-btn" onClick={() => logout({ returnTo: window.location.origin })}> Log Out </button></li>
-              ):(
-                <li><button className="login-btn" onClick={() => loginWithRedirect()}>Log In</button></li>
-              )
-            }
-            
-        </ul>
-      </div>
-const NavigationBar = () => {
-  return (
     <div>
       <ul className="nav-ul">
         <li>
-          <Link key={0} className="brand-name" to="/TasteBuds">
-            TasteBuds
-          </Link>
-          <Link key={1} to="/Favourites">
-            Favourites
-          </Link>
-          <Link key={2} to="/Itemcart">
-            Itemcart (2)
-          </Link>
-          <Link key={3} to="/MyOrders">
-            My Orders
-          </Link>
+          {isAuthenticated ? (
+            <>
+              <Link key={0} className="brand-name" to="/TasteBuds">
+                TasteBuds
+              </Link>
+              <Link key={1} to="/Favourites">
+                Wish-List
+              </Link>
+              <Link key={2} to="/Itemcart">
+                Itemcart (2)
+              </Link>
+              <Link key={3} to="/MyOrders">
+                My Orders
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link key={0} className="brand-name" to="/TasteBuds">
+                TasteBuds
+              </Link>
+            </>
+          )}
         </li>
+        {isAuthenticated && (
+          <li>
+            {" "}
+            <p className="username">&nbsp;&nbsp; User : {user.name} </p>
+          </li>
+        )}
+        <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+        {isAuthenticated ? (
+          <li>
+            <button
+              className="logout-btn"
+              onClick={() => logout({ returnTo: window.location.origin })}>
+              {" "}
+              Log Out{" "}
+            </button>
+          </li>
+        ) : (
+          <li>
+            <button className="login-btn" onClick={() => loginWithRedirect()}>
+              Log In
+            </button>
+          </li>
+        )}
       </ul>
     </div>
   );
 };
 
-export default NavigationBar;
+export default Navigation;
